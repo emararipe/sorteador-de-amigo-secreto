66a4bb724c80fb22a9b87af1db2634f8d7ea3629
emararipe <emararipe@gmail.com>
2023-07-18 19:21:49 -0300

    Commit inicial

---
+++ src/components/Formulario/Formulario.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Formulario from "./Formulario";
+
+// Jest
+
+test('participantes não podem ser adicionados se o input estiver vazio, ', () => {
+
+    render(<Formulario />)
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+
+    const botao = screen.getByRole('button')
+
+    expect(input).toBeInTheDocument()
+
+    expect(botao).toBeDisabled()
+
+})
