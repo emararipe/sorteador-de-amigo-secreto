6e1e48acfb60559573111cffceea48a9322d847e
emararipe <emararipe@gmail.com>
2023-07-18 22:29:33 -0300

    Create: teste nomes duplicados

--- src/components/Formulario/Formulario.test.tsx
+++ src/components/Formulario/Formulario.test.tsx
@@ -41,3 +41,32 @@ test('adicionar um participante se o input estiver preenchido', () => {
   expect(input).toHaveFocus()
   expect(input).toHaveValue("")
 })
+
+
+test('a mensagem de erro para nomes duplicados', () => {
+  
+  render(
+      <RecoilRoot>
+          <Formulario />
+      </RecoilRoot>)
+  const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  const botao = screen.getByRole('button')
+  fireEvent.change(input, {
+      target: {
+          value: 'Pessoa'
+      }
+  })
+  fireEvent.click(botao)
+  fireEvent.change(input, {
+      target: {
+          value: 'Pessoa'
+      }
+  })
+  fireEvent.click(botao)
+  // até agora, simulamos a situação que levaria ao erro
+
+  const mensagemErro = screen.getByRole('alert')
+
+  expect(mensagemErro.textContent).toBe('Esse nome já consta na lista. Nomes duplicados não são permitidos.')
+
+})
