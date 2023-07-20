f27b5329b2ce57466154ed593c5289d316586f25
emararipe <emararipe@gmail.com>
2023-07-20 19:28:53 -0300

    Update: sorteio test para exibir resultado

--- src/pages/Sorteio.test.tsx
+++ src/pages/Sorteio.test.tsx
@@ -1,4 +1,4 @@
-import { render,screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { RecoilRoot } from 'recoil'
 import Sorteio from './Sorteio'
 import { useListaParticipantes } from '../state/hooks/useListaParticipantes'
@@ -16,7 +16,7 @@ describe('página de sorteio', () => {
     (useListaParticipantes as jest.Mock).mockReturnValue(listaFake)
   })
 
-  test('todos os participantes podem serexbidos para iniciar o sorteio', () => {
+  test('todos os participantes podem ser exbidos para iniciar o sorteio', () => {
     render(
       <RecoilRoot>
         <Sorteio />
@@ -25,7 +25,29 @@ describe('página de sorteio', () => {
 
     const opcoes = screen.getAllByRole('option')
     expect(opcoes).toHaveLength(listaFake.length)
+  })
 
+  test('o participante sorteado é exibido', () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    )
+
+    const select = screen.getByPlaceholderText('Selecione o seu nome')
     
+    fireEvent.change(select, {
+      target: {
+        value: listaFake[0]
+      }
+    })
+
+    const botaoSortear = screen.getByRole('button')
+
+    fireEvent.click(botaoSortear)
+
+    const amigoSorteado = screen.getByRole('alert')
+
+    expect(amigoSorteado).toBeInTheDocument()
   })
 })
