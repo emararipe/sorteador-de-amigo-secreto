15a2f37c550ae532dc3ef3a61ee94c02fd0429f2
emararipe <emararipe@gmail.com>
2023-07-20 20:05:11 -0300

    Create: hook resultado sorteio + Upadate: sorteio para implementar hook

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -1,14 +1,40 @@
+import { useState } from 'react'
 import { useListaParticipantes } from '../state/hooks/useListaParticipantes'
+import { useResultadoSorteio } from '../state/hooks/useResultadoSorteio'
 
 const Sorteio = () => {
   const lista: string[] = useListaParticipantes()
+
+  const resultadoSorteio = useResultadoSorteio()
+
+  const [participanteDaVez, setParticipanteDaVez] = useState('')
+  const [amigoSorteado, setAmigoSorteado] = useState('')
+
+  const sortear = (evento:React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const amigoSecreto = resultadoSorteio.get(participanteDaVez) 
+    if(amigoSecreto)
+    setAmigoSorteado(amigoSecreto)  
+  }
+
   return (
     <section>
-      <form>
-        {lista.map((participante) => (
-          <option key={participante}>{participante}</option>
-        ))}
+      <form onSubmit={evento => sortear(evento)}>
+        <select 
+          name='participanteDaVez'
+          id='participanteDaVez'
+          placeholder='Selecione o seu nome'
+          value={participanteDaVez}
+          onChange={evento => setParticipanteDaVez(evento.target.value)}
+          required
+        >
+          {lista.map((participante) => (
+            <option  key={participante}>{participante}</option>
+          ))}
+        </select> 
+        <button type='submit'>Sortear</button>
       </form>
+      <p>{amigoSorteado}</p>
     </section>
   )
 }
